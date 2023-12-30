354cb3180fd0d783df9df80056772a2f552c6123
Nader Zaman <naderzaman50@gmail.com>
2023-12-30 20:03:52 +0500

    Create UseReducerHook

---
+++ src/Hooks-Pedro/UseReducer/UseReducerHook.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UseReducerHook = () => {
+    const [count, setCount] = useState(0);
+    const [showText, setShowText] = useState(true)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UseReducerHook
