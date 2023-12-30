27015c0a47d76597a3885a7343b53fbb2008424e
Nader Zaman <naderzaman50@gmail.com>
2023-12-30 19:57:08 +0500

    UseState Hook
    UseState Hook using input value example

--- src/Hooks-Pedro/UseState/UseStateHook.js
+++ src/Hooks-Pedro/UseState/UseStateHook.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const UseStateHook = () => {
     const [counter, setCounter] = useState(0);
+    const [inputvalue, setInputvalue] = useState('Nader')
     const increment = ()=>{
         setCounter(counter+1)
     }
@@ -10,12 +11,22 @@ const UseStateHook = () => {
     }
   return (
     <div>
-      
+      <h1>Use State Hook Counter </h1>
       <button onClick={increment}>Increment </button>
       {" "}
       {counter}
       {" "}
       <button onClick={decrement}>Decrement</button>
+      <div>
+        <h1>UseState Hook Input Field</h1>
+        <div>
+            <input type="text" placeholder='Enter your Name here' 
+            onChange ={(e) => setInputvalue (e.target.value)}
+            />
+            {inputvalue}
+        </div>
+
+      </div>
     </div>
   )
 }
