a5d8e71b1b6b79324c517f6b51a5003325b7d1f9
Nader Zaman <naderzaman50@gmail.com>
2023-12-31 11:50:11 +0500

    Update UseEffectHook.js

--- src/Hooks-Pedro/UseEffect/UseEffectHook.js
+++ src/Hooks-Pedro/UseEffect/UseEffectHook.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 // 1) useEffect hook is used for handling things outside the scope of React, like manually manipulating the DOM , make an API call, access to localStorage, etc. 
 // 2) useEffect hook is also used to keep different states in sync.
 // 3) importance of the cleanUp funciton when using useEffect hook, to avoid memory leak on components that are not unmounted by React.
 const UseEffectHook = () => {
+  const [data, setData] = useState('')
   useEffect(() => {
-    console.log("Hello World");
-  return () => {
+    axios
+    .get("https://jsonplaceholder.typicode.com/comments")
+    .then((response) => {
+    // console.log(response.data);
+    setData(response.data[9].email)
+
+    });  return () => {
       
     }
   }, [])
@@ -14,6 +20,7 @@ const UseEffectHook = () => {
   return (
     <div>
       <h1>UseEffect Hook </h1>
+      <p> {data}</p>
     </div>
   )
 }
