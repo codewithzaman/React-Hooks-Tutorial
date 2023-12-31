88c218027fba1b01d84e820062f68702324ab350
Nader Zaman <naderzaman50@gmail.com>
2023-12-31 10:09:34 +0500

    Update UseEffectHook.js

--- src/Hooks-Pedro/UseEffect/UseEffectHook.js
+++ src/Hooks-Pedro/UseEffect/UseEffectHook.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+// 1) useEffect hook is used for handling things outside the scope of React, like manually manipulating the DOM , make an API call, access to localStorage, etc. 
+// 2) useEffect hook is also used to keep different states in sync.
+// 3) importance of the cleanUp funciton when using useEffect hook, to avoid memory leak on components that are not unmounted by React.
 const UseEffectHook = () => {
   return (
     <div>
