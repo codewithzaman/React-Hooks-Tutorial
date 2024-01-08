711c74ef4ad71c9b26751c6dcc4692a0416edcc9
codewithzaman <naderzaman50@gmail.com>
2024-01-08 20:54:04 +0500

    Update UseRefCodvolution.js

--- src/Hooks-Pedro/UseRefHook/UseRefCodvolution.js
+++ src/Hooks-Pedro/UseRefHook/UseRefCodvolution.js
@@ -2,11 +2,19 @@ import React, { useRef } from 'react';
 // useRef handle the color, focus and value in functional component.
 
 const UseRefCodvolution = () => {
-    const inputRef = useRef(null)
+    const inputRef = useRef(null);
+    const HandleInput = ()=>{
+console.warn('Function called');
+// inputRef.current.value = "1000";
+// inputRef.current.focus();
+// inputRef.current.style.color = "red"
+inputRef.current.style.display = "none"
+    }
   return (
-    <div>
-        UseRefCodvolution
-        <input type="text" ref={inputRef} />
+    <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-4xl'>UseRefCodvolution</h1>
+        <input type="text" ref={inputRef} className='border-gray-400 border my-4' />
+        <button onClick={HandleInput}>Handle input</button>
         </div>
   
   )
