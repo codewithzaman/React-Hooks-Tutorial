bd963ab01bdb6aaa3a0aba5cdeaeadf05c9b2f89
codewithzaman <naderzaman50@gmail.com>
2024-01-08 19:25:55 +0500

    Add tailwind config file

--- src/Hooks-Pedro/UseEffect/UseEffectHook.js
+++ src/Hooks-Pedro/UseEffect/UseEffectHook.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 // 2) useEffect hook is also used to keep different states in sync.
 // 3) importance of the cleanUp funciton when using useEffect hook, to avoid memory leak on components that are not unmounted by React.
 const UseEffectHook = () => {
-  const [data, setData] = useState('')
+  const [data, setData] = useState('');
+  const [count, setCount] = useState(0)
   useEffect(() => {
     axios
     .get("https://jsonplaceholder.typicode.com/comments")
@@ -16,12 +17,15 @@ const UseEffectHook = () => {
     });  return () => {
       
     }
-  }, [data])
+  }, [])
   
   return (
-    <div>
-      <h1>UseEffect Hook </h1>
-      <p> {data}</p>
+    <div className='flex flex-col justify-center items-center bg-black text-white'>
+      <h1 className='mt-6'>UseEffect Hook </h1>
+      <h3 className='mt-4'>{data}</h3><br/>
+      <h2 className='text-4xl font-semibold'>{count}</h2><br/>
+      <button onClick={()=>setCount(count+1)}>Count</button>
+
     </div>
   )
 }
