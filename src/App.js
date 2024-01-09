1295f717672cd00aa2e22dfb35ea361fde62fa96
codewithzaman <naderzaman50@gmail.com>
2024-01-09 19:04:11 +0500

    Update UseEffectLayout hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import UseLayoutEffecthook from './Hooks-Pedro/UseLayoutEffect/UseLayoutEffecthook';
 import UseRefWebDevSimplified from './Hooks-Pedro/UseRefHook/UseRefWebDevSimplified';
 // import UseRefCodvolution from './Hooks-Pedro/UseRefHook/UseRefCodvolution';
 // import UseEffectHook from './Hooks-Pedro/UseEffect/UseEffectHook';
@@ -27,7 +28,8 @@ function App() {
       {/* <UseEffectHook/> */}
       {/* <UseRefHook/> */}
       {/* <UseRefCodvolution/> */}
-      <UseRefWebDevSimplified/>
+      {/* <UseRefWebDevSimplified/> */}
+      <UseLayoutEffecthook/>
     </div>
   );
 }
--- src/Hooks-Pedro/UseLayoutEffect/UseLayoutEffecthook.js
+++ src/Hooks-Pedro/UseLayoutEffect/UseLayoutEffecthook.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect } from 'react';
+// If we use both hooks Useeffect and uselayouteffect at the same time then uselayouteffect will render first
+
 
 const UseLayoutEffecthook = () => {
+    useLayoutEffect(() => {
+        console.log('UseLayoutEffect');
+    
+    },[] )
+    useEffect(() => {
+     console.log('UseEffect ');
+    }, [])
+    
   return (
-    <div>UseLayoutEffecthook</div>
+    <div>
+
+    </div>
   )
 }
 
