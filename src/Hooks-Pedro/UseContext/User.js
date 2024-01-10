7a75280736e3815b2e912007a53193dd15500b91
codewithzaman <naderzaman50@gmail.com>
2024-01-10 21:09:12 +0500

    Update User.js

--- src/Hooks-Pedro/UseContext/User.js
+++ src/Hooks-Pedro/UseContext/User.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const User = () => {
+const User = ({userName}) => {
   return (
-    <div>User</div>
+    <div>
+        <h1>User: {userName}</h1>
+    </div>
   )
 }
 
