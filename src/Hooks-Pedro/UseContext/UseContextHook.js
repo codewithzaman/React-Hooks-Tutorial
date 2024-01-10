866c8d13659e8eb16ade9b684c3813927a9fed8c
codewithzaman <naderzaman50@gmail.com>
2024-01-10 21:15:30 +0500

    Update UseContextHook.js
    Added tailwindCss

--- src/Hooks-Pedro/UseContext/UseContextHook.js
+++ src/Hooks-Pedro/UseContext/UseContextHook.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './Login';
 import User from './User';
 
@@ -6,7 +6,7 @@ import User from './User';
 function UseContextHook() {
     const [userName, setUserName] = useState('')
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center'>
         <h1>UseContextHook</h1>
         <Login setUserName={setUserName}/>
         <User userName={userName}/>
