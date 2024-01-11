11f730d780843834a3a4ba210d6f6c8f58901143
codewithzaman <naderzaman50@gmail.com>
2024-01-11 21:39:19 +0500

    Update User.js

--- src/Hooks-Pedro/UseContext/User.js
+++ src/Hooks-Pedro/UseContext/User.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import { AppContext } from './UseContextHook'
 const User = () => {
+  const {userName} = useContext(AppContext);
   return (
     <div>
-        <h1 className='text-4xl font-bold'>User: </h1>
+        <h1 className='text-4xl font-bold'>User:{userName} </h1>
     </div>
   )
 }
