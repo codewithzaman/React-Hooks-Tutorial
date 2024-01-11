9bad5b809e621e0314ed025e2837206686810c03
codewithzaman <naderzaman50@gmail.com>
2024-01-11 21:31:12 +0500

    Update UseContextHook.js

--- src/Hooks-Pedro/UseContext/UseContextHook.js
+++ src/Hooks-Pedro/UseContext/UseContextHook.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState,createContext } from 'react';
 import Login from './Login';
 import User from './User';
-
+const AppContext = createContext(null);
 
 function UseContextHook() {
     const [userName, setUserName] = useState('')
   return (
     <div className='flex flex-col justify-center items-center'>
         <h1>UseContextHook</h1>
-        <Login setUserName={setUserName}/>
-        <User userName={userName}/>
+        <Login />
+        <User />
 
     </div>
   )
