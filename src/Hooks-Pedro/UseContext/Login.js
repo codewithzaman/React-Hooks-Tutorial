1ab3077a8d07edcd3696d04449439457da03721f
codewithzaman <naderzaman50@gmail.com>
2024-01-11 21:39:22 +0500

    Update Login.js

--- src/Hooks-Pedro/UseContext/Login.js
+++ src/Hooks-Pedro/UseContext/Login.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { AppContext } from './UseContextHook'
 
 function Login() {
+  const {setUserName} = useContext(AppContext);
+
   return (
     <div className='mb-4 mt-4 border border-gray-400'>
-        <input type="text" onChange={(event)=>(event.target.value) }/>
+        <input type="text" onChange={(event)=>setUserName(event.target.value) }/>
     </div>
   )
 }
